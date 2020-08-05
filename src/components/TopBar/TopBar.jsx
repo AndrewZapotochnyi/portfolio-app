04bfd872ea2c635c11dbd10f1fd45306e16485c2
Andrew Zapotochnyi <andrewzapotochnyi@icloud.com>
2020-08-05 15:36:34 -0400

    Dock Icons Styled

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -2,6 +2,21 @@ import React from 'react';
 import logo from '../../styles/images/Apple_Logo.png';
 import styled from 'styled-components'
 
+
+const Dropdown = styled.div`
+        color: black;
+        font-size: 15px;
+        display: flex;
+        flex-flow: center; 
+        background: rgba(249, 249, 249, 0.7);
+        backdrop-filter: blur(30px);
+        padding: 7px 12px;
+        border-radius: 10px;
+        position: absolute;
+        bottom: -80%;
+        box-shadow: 0 0 3px #999999;
+    `;
+
 let TopBar = () => {
 
   return (
@@ -11,6 +26,7 @@ let TopBar = () => {
       <div className="top-menu-items">Finder</div>
       <div className="top-menu-items">File</div>
       <div className="top-menu-items">Edit</div>
+      <Dropdown />
     
     </div>);
 }
--- src/components/icons/IconWrapper.jsx
+++ src/components/icons/IconWrapper.jsx
@@ -3,9 +3,10 @@ import CalendarImage from '../../styles/icons/Calendar.svg'
 import styled from 'styled-components'
 
 const IconWrap = styled.img`
-        width: 65px;
-        height: 65px;
-        margin-right: 10px;
+        width: 60px;
+        height: 60px;
+        // margin-right: 10px;
+        display: flex;
         `;
 
 export default IconWrap;
